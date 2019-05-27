b96803041da7f4706df13c077c48ff45192f5640
15011361103da <luda395353765@163.com>
2019-05-27 17:29:17 +0800

    REM

---
+++ src/components/HelloREM.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="box">
+      哈哈
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HelloREM"
+    }
+</script>
+
+<style  lang="less">
+
+  .box{
+    width: 4rem;
+    height: 6rem;
+    background-color: red;
+    font-size: 16/50rem;
+  }
+</style>
